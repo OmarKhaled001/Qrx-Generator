66ce59d66b5fdbad1841f2b5de07be26bfabdd16
OmarKhaled001 <omarkhaled102002@gmail.com>
2024-05-03 19:58:15 +0200

    taaaaaaaaaaaaaaa

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,12 +10,13 @@ class HomeController extends Controller
     public function getPlans(){
         //check if user auth has subscription
         if(count(Auth()->user()->subscriptions) > 0){
-            $subscription =Auth()->user()->subscriptions->first();
-            //get plan
+            $user   = Auth()->user();
+            $subscription = $user->subscriptions->first();
             $plan_id = $subscription->items->first()->stripe_product;
             $plans = Plan::all()->except('plan_id',$plan_id);
             //retu plan 
             return view('Frontend.index',compact($plans));
+            
         }else{
             $plans = Plan::all();
             //retu plan 
