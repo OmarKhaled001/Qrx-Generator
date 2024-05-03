d7d75df949406d8479af027732041cabc48a969c
OmarKhaled001 <omarkhaled102002@gmail.com>
2024-05-03 13:33:59 +0200

    tesst

--- resources/views/Frontend/index.blade.php
+++ resources/views/Frontend/index.blade.php
@@ -20,8 +20,10 @@
         @include('Frontend.sections.slider')				
         @include('Frontend.sections.about-us')				
         @include('Frontend.sections.qr-create')				
-        @include('Frontend.sections.features')				
-        @include('Frontend.sections.pricing')				
+        @include('Frontend.sections.features')
+        @if( count(Auth()->user()->subscriptions) > 0)				
+        @include('Frontend.sections.pricing')	
+        @endif			
         @include('Frontend.sections.contact-us')				
         @include('Frontend.main.footer')
     </div>		
