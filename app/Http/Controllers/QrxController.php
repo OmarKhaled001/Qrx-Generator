e7b2c0008e7bf749176de8063ef8df3389352cbe
OmarKhaled001 <omarkhaled102002@gmail.com>
2024-05-03 12:52:56 +0200

    plannnnnnnnnnnnnnn

--- app/Http/Controllers/QrxController.php
+++ app/Http/Controllers/QrxController.php
@@ -32,6 +32,7 @@ class QrxController extends Controller
             $subscription = $user->subscriptions;
             $subscription = $user->subscriptions->first();
             $plan_id = $subscription->items->first()->stripe_product;
+            $plan = Plan::where('plan_id',$plan_id)->first()->get();
             $now = Carbon::now();
             $startDate =  Carbon::parse($subscription->created_at);
             $endDate = $startDate->addYear(1);
