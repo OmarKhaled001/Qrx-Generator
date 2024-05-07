9cb499b5c085f9cdde020f0e125de6576fe1d493
OmarKhaled001 <omarkhaled102002@gmail.com>
2024-05-07 13:41:34 +0200

    testtt

--- resources/views/Dashboard/sections/errors/plan-subscripe.blade.php
+++ resources/views/Dashboard/sections/errors/plan-subscripe.blade.php
@@ -4,7 +4,7 @@
             <div class="mt-2 text-center">
                 <h3 class="mt-2 fw-semibold">Sorry for you</h3>
                 <p class="text-muted mb-4">You are not subscribed to any plan yet, and you must choose a suitable plan and subscribe to it</p>
-                <a class="btn btn-primary" https://qrx-generator.zat.pw/#pricing-1>Our Plans</a>
+                <a class="btn btn-primary"  href=" https://qrx-generator.zat.pw/#pricing-1">Our Plans</a>
                 <div class="error-img text-center px-5">
                     <img src="{{asset('assets-dashboard/images/auth/offline-page.gif')}}" class="img-fluid" alt="">
                 </div>
