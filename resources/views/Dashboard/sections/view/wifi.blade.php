72eb47f2fb80a0637310bbe99163836fa5a8e5be
OmarKhaled001 <omarkhaled102002@gmail.com>
2024-05-07 15:35:08 +0200

    wifi

--- resources/views/Dashboard/sections/view/wifi.blade.php
+++ resources/views/Dashboard/sections/view/wifi.blade.php
@@ -31,5 +31,5 @@
 <div class="d-flex align-items-center">
 <input readonly  id="foo" class="form-control" value="{{$qrxCode->wifi->encryption}}" />
 </div>
-<a class="btn btn-success h1 text-center mt-5" href="wifi:ssid={{$qrxCode->wifi->wireless_ssid}}?&encryption={{$qrxCode->wifi->encryption}}?&password={{$qrxCode->wifi->password}}?&H=false" >Connect</a>
+<a class="btn btn-success h1 text-center mt-5" href="WIFI:S:{{$qrxCode->wifi->wireless_ssid}};T:{{$qrxCode->wifi->encryption}};P:{{$qrxCode->wifi->password}};H:false" >Connect</a>
 @endif
