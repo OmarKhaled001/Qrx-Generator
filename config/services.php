8fb0f9ea3ddaafd31d3a3a7c5d45a5a2d4d4b7d0
OmarKhaled001 <omarkhaled102002@gmail.com>
2024-05-09 12:37:02 +0200

    provider

--- config/services.php
+++ config/services.php
@@ -38,7 +38,7 @@ return [
     'facebook' => [    
         'client_id' => env('FACEBOOK_CLIENT_ID'),  
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),  
-        'redirect' =>  env('APP_URL').'/auth/google/callback',
+        'redirect' =>  env('APP_URL').'/auth/facebook/callback',
     ],
 
 ];
