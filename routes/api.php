637e8fc561051b395a5fabc249c54625dd2d5372
OmarKhaled001 <omarkhaled102002@gmail.com>
2024-05-13 11:01:03 +0200

    go

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SocialiteController;
+use App\Http\Controllers\api\SocialiteController;
 
 /*
 |--------------------------------------------------------------------------
