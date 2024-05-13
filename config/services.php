6515556d6835157801a1674fd145b6d9ef962c87
OmarKhaled001 <omarkhaled102002@gmail.com>
2024-05-13 14:27:40 +0200

    provider

--- config/services.php
+++ config/services.php
@@ -33,7 +33,7 @@ return [
     'google' => [    
         'client_id' => env('GOOGLE_CLIENT_ID'),  
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),  
-        'redirect' => env('APP_URL').'/auth/google/callback',
+        'redirect' => env('APP_URL').'/api/auth/google/callback',
     ],
     'facebook' => [    
         'client_id' => env('FACEBOOK_CLIENT_ID'),  
