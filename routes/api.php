12dd58b6ce3ba691bd3c98f99b68da79b5f6f4f3
OmarKhaled001 <omarkhaled102002@gmail.com>
2024-05-13 12:53:59 +0200

    go

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,3 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('auth')->name('socialite.')->controller(SocialiteController::class)->group(function(){
-    Route::get('/{provider}/callback','callback')->name('callback');
-    Route::get('/{provider}/redirect','redirect')->name('redirect');
-});
