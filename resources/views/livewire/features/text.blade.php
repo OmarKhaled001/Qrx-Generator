32088dc6f2d80cd503b4eff99b42564aa6e01e88
OmarKhaled001 <omarkhaled102002@gmail.com>
2024-05-30 00:01:36 +0200

    go

--- resources/views/livewire/features/text.blade.php
+++ resources/views/livewire/features/text.blade.php
@@ -1,5 +1,5 @@
 @if($tab == 'text')
-    <div class="p-3">
+    <div class="p-3 ">
         <label class="h6 my-2">Text :</label>
         <input type="text" class="form-control my-1"  placeholder="write text" wire:model="text">
     </div>
